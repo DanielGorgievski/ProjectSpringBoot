95e845c933b7c1d79a6e0fd426943bb20cae56bc
Daniel <134344835+DanielGorgievski@users.noreply.github.com>
2025-05-31 21:34:50 +0200

    .

--- src/main/java/com/chatapi/config/DataInitializer.java
+++ src/main/java/com/chatapi/config/DataInitializer.java
@@ -19,9 +19,9 @@ public class DataInitializer {
     CommandLineRunner initData(UserRepository userRepository, RoomRepository roomRepository, MessageRepository messageRepository) {
         return args -> {
 
-            User user1 = new User("Alice", "alice@example.com");
-            User user2 = new User("Bob", "bob@example.com");
-            User user3 = new User("Charlie", "charlie@example.com");
+            User user1 = new User("Daniel", "daniel@gmail.com");
+            User user2 = new User("Sara", "sara@yahoo.com");
+            User user3 = new User("Tino", "tino@outlook.com");
 
             userRepository.saveAll(Arrays.asList(user1, user2, user3));
 
@@ -31,7 +31,6 @@ public class DataInitializer {
 
             roomRepository.saveAll(Arrays.asList(room1, room2, room3));
 
-            // Add users to rooms
             room1.getParticipants().add(user1);
             room1.getParticipants().add(user2);
             room1.getParticipants().add(user3);
@@ -44,15 +43,15 @@ public class DataInitializer {
 
             roomRepository.saveAll(Arrays.asList(room1, room2, room3));
 
-            Message msg1 = new Message("Hey, how's everyone?", user1, room1);
-            Message msg2 = new Message("I'm doing great! What about you?", user2, room1);
-            Message msg3 = new Message("Just joined! What's going on here?", user3, room1);
+            Message msg1 = new Message("Hey, how is everybody?", user1, room1);
+            Message msg2 = new Message("Im doing great, What about you?", user2, room1);
+            Message msg3 = new Message("What's going on here?", user3, room1);
 
-            Message msg4 = new Message("Anyone up for some coding challenge?", user1, room2);
-            Message msg5 = new Message("Sure! Let's solve some problems.", user2, room2);
+            Message msg4 = new Message("Anyone wanna go out?", user1, room2);
+            Message msg5 = new Message("Sure! lets go out somewhere.", user2, room2);
 
-            Message msg6 = new Message("Who wants to play some games?", user2, room3);
-            Message msg7 = new Message("Count me in!", user3, room3);
+            Message msg6 = new Message("Who wants to play some video games?", user2, room3);
+            Message msg7 = new Message("Me!", user3, room3);
 
             messageRepository.saveAll(Arrays.asList(msg1, msg2, msg3, msg4, msg5, msg6, msg7));
         };
